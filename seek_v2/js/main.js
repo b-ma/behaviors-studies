b7339546b0af288691d11dd0710761384e9ed19b
b-ma <benjamin.matuszewski@gmail.com>
2014-06-21 01:35:24 +0200

    seek v2 - alt draw

---
+++ seek_v2/js/main.js
@@ -0,0 +1,46 @@
+var SHAPE_COUNT = 20;
+var shapes = [];
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = ctx.canvas.width = 500;
+var height = ctx.canvas.height = 200;
+
+
+for (var i = 0; i < SHAPE_COUNT; i++) {
+    var shape = new Shape(Math.random() * ctx.canvas.width, Math.random() * ctx.canvas.height);
+    shapes.push(shape);
+}
+var mouse = new Vector;
+
+// canvas.addEventListener('click', function(e) {
+(function updateTarget() {
+    mouse.x = Math.random() * width;
+    mouse.y = Math.random() * height;
+    setTimeout(updateTarget, 2 * 1000 * Math.random() + 1500);
+}());
+// }, false);
+
+(function loop() {
+    // add layer to scene
+    ctx.save();
+    ctx.fillStyle = '#000000';
+    ctx.globalAlpha = 0.05;
+    ctx.fillRect(0, 0, width, height);
+    ctx.restore();
+
+    // update scene
+    for (var i in shapes) {
+        shapes[i].update(width, height, mouse);
+        shapes[i].display(ctx);
+    }
+
+    //  draw mouse position
+    //  ctx.beginPath();
+    //  ctx.fillStyle = '#ff0000';
+    //  ctx.arc(mouse.x, mouse.y, 2, 0, Math.PI * 2, false)
+    //  ctx.fill();
+
+    requestAnimationFrame(loop);
+}());
---
+++ seek_v2/js/shape.js
@@ -0,0 +1,52 @@
+var Shape =  function(x, y) {
+    this.position = new Vector(x, y);
+    this.velocity = new Vector(Math.random() * 2 - 1, Math.random() * 2 - 1);
+
+    this.MAX_VELOCITY = 2;
+    this.mass = Math.random() * 30;
+}
+
+extend(Shape.prototype, {
+    bounce: function(w, h) {
+        if (this.position.x > w) { this.position.x = 0; }
+        if (this.position.x < 0) { this.position.x = w; }
+        if (this.position.y > h) { this.position.y = 0; }
+        if (this.position.y < 0) { this.position.y = h; }
+    },
+
+    update: function(w, h, mouse) {
+        this.velocity.normalize();
+
+        var desiredVelocity = Vector.substract(mouse, this.position);
+        var steering = Vector.substract(desiredVelocity, this.velocity);
+
+        steering
+            .normalize()
+            .divide(this.mass);
+
+        this.velocity
+            .add(steering)
+            .normalize(this.MAX_VELOCITY);
+
+        this.position.add(this.velocity);
+        this.bounce(w, h);
+    },
+
+    display: function(ctx) {
+        ctx.save();
+
+        ctx.translate(this.position.x, this.position.y);
+        ctx.rotate(this.velocity.direction());
+
+        // draw shape
+        ctx.beginPath();
+        ctx.fillStyle = '#ffffff';
+        // ctx.moveTo(-4, 4);
+        // ctx.lineTo(-4, -4);
+        // ctx.lineTo(6, 0);
+        ctx.arc(0, 0, 2, 0, Math.PI * 2, false);
+        ctx.fill();
+
+        ctx.restore();
+    }
+});
